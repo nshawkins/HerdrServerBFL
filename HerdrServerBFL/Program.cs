53e5ea92c551c1e6961664301f1d8466a18f0b63
nshawkins <hawkinsn67@gmail.com>
2021-05-01 20:25:01 -0400

    db

--- HerdrServerBFL/Program.cs
+++ HerdrServerBFL/Program.cs
@@ -35,8 +35,8 @@ namespace HerdrServer
             connectionString = "Server=" + server + ";" + "Database=" + 
             database + ";" + "User Id=" + uid + ";" + "Password=" + password + ";";*/
 
-            //server = "tcp:3.23.163.170,3306";
-            server = "tcp:127.0.0.1,3306";
+            server = "tcp:3.23.163.170,3306";
+            //server = "tcp:127.0.0.1,3306";
             //Library = "DBMSSOCN"; 
             database = "herdrdb";
 
